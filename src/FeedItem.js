2adabd8d14d35ac5968ea2b778d64420a6466057
Johnny Santiago <johnnyin2a@gmail.com>
2021-11-11 13:52:49 -0500

    Update styling for upvote functionality.

--- src/FeedItem.js
+++ src/FeedItem.js
@@ -1,10 +1,13 @@
 import { GraphQLClient, gql } from "graphql-request";
 import moment from 'moment';
+import { useState } from "react";
 
+import checkmark from './images/checkmark.svg';
 import thumbsUp from './images/thumbsUp.svg';
 import './FeedItem.css';
 
 export default function FeedItem(props) {
+  const [isUpVoted, setIsUpVoted] = useState(false);
   const mutation = gql`
     mutation upVote($id: ID!) {
       upvote(id: $id) {
@@ -24,18 +27,38 @@ export default function FeedItem(props) {
       console.log("Error: " + error);
     }
     
+    setIsUpVoted(true);
     props.onUpVote(props.id);
   }
   
+  const voteCountClassName = isUpVoted ? 'up-voted-vote-count' : 'vote-count';
+  const voteIconClassName = isUpVoted ? 'vote-icon' : 'vote-icon-with-low-opacity';
+  const voteStatusIcon = isUpVoted ? checkmark : thumbsUp;
+  const voteStatusAltText = isUpVoted ? 'checkmark' : 'thumbs up';
+
   return (
     <li className="item">
       <div className="item-number">{props.number}</div>
       <div className="item-text"> 
         <h2 className="description">{props.description}</h2>
         <p className="url">{props.url}</p>
-        <p className="votes-and-created-at">{props.votes} votes by {props.postedBy.name} {moment(props.createdAt).fromNow()}</p>
+        <p 
+          className="votes-and-created-at"
+        >
+          <span className={voteCountClassName}>{props.votes}</span> votes by&nbsp; 
+          {props.postedBy.name}&nbsp; 
+          {moment(props.createdAt).fromNow()}
+        </p>
       </div>
-      <button className="vote-button" disabled={props.disabled} onClick={handleClick}><img className="vote-icon" src={thumbsUp} alt="thumbs up" /></button>
+      <button 
+        className="vote-button" 
+        disabled={props.disabled} 
+        onClick={handleClick}>
+          <img className={voteIconClassName} 
+            src={voteStatusIcon} 
+            alt={voteStatusAltText} 
+          />
+      </button>
     </li>
   )
 }
