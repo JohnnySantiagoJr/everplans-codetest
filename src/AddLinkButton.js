0db2acac005ca8784eb1279fefe71590f890e008
Johnny Santiago <johnnyin2a@gmail.com>
2021-11-10 23:45:52 -0500

    Style the AddLinkButton.

--- src/AddLinkButton.js
+++ src/AddLinkButton.js
@@ -1,12 +1,12 @@
-import circle from './images/circle.svg';
+import ellipse from './images/ellipse.png';
 import plusSign from './images/plus-sign.svg';
 import './AddLinkButton.css';
 
 export default function AddLinkButton(props) {
   return (
-    <button disabled={props.disabled}onClick={props.onClick} className="addLinkButton">
-      <img className="buttonBackground" src={circle} alt="circle" />
-      <img className="plusSign" src={plusSign} alt="plus sign" />
+    <button className="button" disabled={props.disabled} onClick={props.onClick}>
+      <img className="button-background" src={ellipse} alt="circle" />
+      <img className="plus-sign" src={plusSign} alt="plus sign" />
     </button>
   )
 } 
