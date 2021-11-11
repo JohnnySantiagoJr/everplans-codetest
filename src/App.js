3393627b1535a30d907f018a3ec4e93356a7ae45
Johnny Santiago <johnnyin2a@gmail.com>
2021-11-11 13:51:02 -0500

    Add routing.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import  Feed from './Feed.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Feed from './Feed.js';
+import LogIn from './LogIn.js';
+import SignUp from './SignUp.js'
 
 function App() {
   return (
+    <BrowserRouter>
       <div className="App">
-        <Feed />
+        <Routes>
+          <Route index element={<Feed />} />
+          <Route exact path="/log-in" element={<LogIn />} />
+          <Route exact path="/sign-up" element={<SignUp />} />
+        </Routes>
       </div>
+    </BrowserRouter>
   );
 }
 
